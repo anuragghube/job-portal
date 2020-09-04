08cce465bd1ca7be18aa0ff36af6d8acf83bf1c1
anuragghube <arghube@gmail.com>
2020-09-04 08:40:13 -0700

    added new job filter and job list modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobFilterComponent } from './module/job-filter/job-filter.component';
+import { JobListComponent } from './module/job-list/job-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: JobListComponent },
+  { path: '**', component: JobFilterComponent } // If no matching route found, go back to home route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobFilterComponent } from './module/job-filter/job-filter.component';
+import { JobListComponent } from './module/job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobFilterComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
